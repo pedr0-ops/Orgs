a34ded76350cd01a5be2904063760a3f26967205
pedr0-ops <pedr0402018@outlook.com>
2023-06-29 22:32:44 -0300

    refactor: add screem inital of app

---
+++ Orgs/src/components/Top/Top.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import logo from '../../assets/logo.png';
+
+const Top = () => {
+  return (
+    <View style={style.top}>
+      <Image source={logo} style={style.img} />
+      <Text style={style.wellCome}>Olá Pedro</Text>
+      <Text style={style.legend}>Encontre os melhores produtos</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  top: {
+    backgroundColor: '#F6F6F6',
+    padding: 16,
+  },
+  img: {
+    width: 70,
+    height: 28,
+  },
+  wellCome: {
+    marginTop: 24,
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: 'bold',
+  },
+  legend: {
+    fontSize: 16,
+    lineHeight: 26,
+  },
+});
+
+export default Top;
---
+++ Orgs/src/screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import Top from '../../components/Top/Top';
+
+const Home = () => {
+  return (
+    <View>
+      <Top />
+    </View>
+  );
+};
+
+export default Home;
